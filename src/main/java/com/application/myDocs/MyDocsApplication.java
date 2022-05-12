3cb00fa852ec4c30bd3da0c73e22fc46d1bc6604
DenisaPlai <plai.denisa2000@yahoo.com>
2022-05-12 17:51:36 +0300

    first commit

---
+++ src/main/java/com/application/myDocs/MyDocsApplication.java
@@ -0,0 +1,13 @@
+package com.application.myDocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDocsApplication.class, args);
+	}
+
+}
